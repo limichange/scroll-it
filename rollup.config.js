af8918ccc27388ba5996df5d0f5850b1fc9179c3
limichange <limichange@hotmail.com>
2018-03-03 21:24:58 +0800

    remove unuseful code

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 import createConfig from './createConfig'
 
-const version = process.env.VERSION || require('./package.json').version
-const name = 'scroll-it'
-
 export default createConfig({
-  name,
-  author: "Limichange",
-  version
+  name: 'scroll-it',
+  author: 'Limichange',
+  version: process.env.VERSION || require('./package.json').version
 })
